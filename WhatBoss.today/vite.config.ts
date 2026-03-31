4d7c3a4424c1f504ed8828452731c9524134853b
courtneytruong <courtneybtruong@gmail.com>
2026-03-31 16:22:50 -0400

    changed repo name in vite.config.ts

--- WhatBoss.today/vite.config.ts
+++ WhatBoss.today/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bounty-board/', // must match your repo name exactly
+  base: '/WhatBoss.today/',
   plugins: [react()],
 })
